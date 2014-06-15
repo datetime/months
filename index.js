2b952096e29bb2fa79f6b1fd730bf5206bba0763
jonschlinkert <dev@sellside.com>
2014-06-15 15:10:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * months <https://github.com/jonschlinkert/months>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+module.exports = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
