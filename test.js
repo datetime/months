c3ba33e4f2c76a8a827c095ced1e78a0c221c689
doowb <brian.woodward@gmail.com>
2017-05-25 12:08:26 -0400

    add tests

---
+++ test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('mocha');
+var assert = require('assert');
+var months = require('./');
+
+describe('months', function() {
+  it('should export an array', function() {
+    assert.equal(Array.isArray(months), true);
+    assert.equal(months[8], 'September');
+  });
+
+  it('should have abbreviations', function() {
+    assert.equal(Array.isArray(months.abbr), true);
+    assert.equal(months.abbr[8], 'Sep');
+  });
+
+  it('should have Italian translations', function() {
+    assert.equal(Array.isArray(months.it), true);
+    assert.equal(months.it[8], 'Settembre');
+    assert.equal(Array.isArray(months.abbr.it), true);
+    assert.equal(months.abbr.it[8], 'Set');
+  });
+});
